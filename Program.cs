544732c9add45d04dfb6c2f24495dc9fb58769bc
m2lawrence <xhtml@live.co.uk>
2016-02-21 20:59:03 +0000

    Using the C# Logical AND and OR Operators
    Using the Logical AND and OR Operators to control the flow of my
    programme.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAndClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 1;
+            char sex = 'f';
+
+            System.Console.WriteLine("Starting tests... (day:{0}, sex:{1})",
+                                      day, sex);
+
+            if (day >= 1 && day <= 7)      //day from 1 to 7? 
+            {
+                System.Console.WriteLine("Day is from 1 to 7");
+            }
+            if (sex == 'm' || sex == 'f')  // Male or female?
+            {
+                System.Console.WriteLine("Sex is male or female.");
+            }
+
+            System.Console.WriteLine("Done with the checks Mike.");
+            System.Console.ReadKey();
+        }
+    }
+}
